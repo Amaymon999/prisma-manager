36b8fca52320c5df6b21a7d8391ca5e41a81cc7e
Amaymon999 <dollaku.deivin7@gmail.com>
2026-02-26 04:10:10 +0100

    Initial commit :wq :wq# Conflicts:

---
+++ app/(app)/layout.tsx
@@ -0,0 +1,10 @@
+import { AppShell } from "@/components/app/app-shell";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+import { redirect } from "next/navigation";
+
+export default async function AppLayout({ children }: { children: React.ReactNode }) {
+  const session = await getServerSession(authOptions);
+  if (!session) redirect("/login");
+  return <AppShell>{children}</AppShell>;
+}
